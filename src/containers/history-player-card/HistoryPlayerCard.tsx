ec8f5e355f82be858bc31731d0bcc449a52f0212
dimkahitrec <armormozg@mail.ru>
2023-07-27 13:53:16 +0300

    added year and description to historyplayercard

--- src/containers/history-player-card/HistoryPlayerCard.tsx
+++ src/containers/history-player-card/HistoryPlayerCard.tsx
@@ -1,29 +1,27 @@
-import styles from './styles.module.scss'
-import { SortableCard, type SortableCardProps } from '../sortable'
-import { type GameCard } from '../../types'
+import styles from "./styles.module.scss"
+import { SortableCard, type SortableCardProps } from "../sortable"
+import { type GameCard } from "../../types"
 
 type HistoryPlayerCardProps = GameCard & SortableCardProps
 
-const HistoryPlayerCard = ({ id, year, title, description }: HistoryPlayerCardProps) => {
+const HistoryPlayerCard = ({
+  id,
+  year,
+  title,
+  description,
+}: HistoryPlayerCardProps) => {
   return (
     <SortableCard id={id} disabled={false}>
       <div className={styles.historyPlayerCard}>
         {/* img */}
         <h3>{title}</h3>
-        {
-          /* code goes here
-            card should display all props
-        */
-        }
+        {year}
+        {description}
       </div>
     </SortableCard>
   )
 }
 
-export type {
-  HistoryPlayerCardProps
-}
+export type { HistoryPlayerCardProps }
 
-export {
-  HistoryPlayerCard
-}
+export { HistoryPlayerCard }
