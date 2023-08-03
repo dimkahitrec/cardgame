4afca0db150c55376be2f8e8899510868bef712c
dimkahitrec <armormozg@mail.ru>
2023-08-03 17:56:18 +0300

    added props: image, year, description to card

--- src/containers/history-player-card/HistoryPlayerCard.tsx
+++ src/containers/history-player-card/HistoryPlayerCard.tsx
@@ -4,10 +4,22 @@ import styles from './styles.module.scss'
 const HistoryPlayerCard = ({ id, year, title, description }: HistoryPlayerCardProps) => {
   return (
     <div className={styles.historyPlayerCard}>
-      {/* img */}
-      <h3>{title}</h3>      
-      {year}
-      {description}     
+      {
+        <div className={styles.fixedphoto}>
+        <img src={'../../photoOfCards/' + title + '.jpg'} className={styles.fixedPhotoBorder} width={245} height={180}/>
+        </div>
+      }
+      <div className={styles.groupTitleYear}>
+        <div className={styles.titleOfCard}>
+          {title}
+        </div>
+        <div className={styles.yearOfCard}>
+          {year}
+        </div>
+      </div>
+      <div className={styles.description}>   
+        {description}
+      </div>
     </div>
   )
 }
