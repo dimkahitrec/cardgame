aee8d34d78bb4338747846d8b5a672e9f00ae741
dimkahitrec <armormozg@mail.ru>
2023-08-15 22:24:13 +0300

    add all_card_deck object with vars

--- src/containers/all-card-deck/AllCardDeck.tsx
+++ src/containers/all-card-deck/AllCardDeck.tsx
@@ -1,21 +1,41 @@
 import { type AllCardDeck as AllCardDeckProps } from './types'
-// import styles from '../all-card-deck/';
-import styles from '../history-player-card/styles.module.scss'
-
+import styles from './styles.module.scss'
+               
 const AllCardDeck = ({ id, title, counter }: AllCardDeckProps) => {
     return (
-        <div className={styles.historyPlayerCard}>
-            {title}
-            {counter}
+        <div className={styles.AllCardDeck}>
+          <div className={styles.AllCardDeckExtend_before1}></div> 
+          <div className={styles.AllCardDeckExtend_before2}></div>   
+          <div className={styles.AllCardDeckExtend_before3}></div>    
+          <div className={styles.textCenter}>{counter}</div>                  
         </div>
     )
 }
 
-export type {
-    AllCardDeckProps
-}
+const AllCardDeckMap: AllCardDeckProps[] = [
+    {
+      id: '1',
+      title: 'Добор',
+      counter: 25
+    }
+]
 
+// display a card-deck
+const AllCardDeckDisplay = () => {
+    return (
+      <div>
+        {AllCardDeckMap.map((deckCards) => (
+          <AllCardDeck key={deckCards.id} {...deckCards} />
+        ))}
+      </div>
+    )
+  }
+
+export {
+    AllCardDeckDisplay
+}
+  
 export default {
-    AllCardDeck
+    AllCardDeckDisplay
 }
 
