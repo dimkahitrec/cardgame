69a5c2ab532cd04dde8684d254fdadb5a61ce42f
dimkahitrec <armormozg@mail.ru>
2023-08-15 22:27:49 +0300

    add display of all_card_deck to game_page

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,10 +1,12 @@
 import { PlayerCardList } from '../../containers/player-card-list'
+import { AllCardDeckDisplay } from '../../containers/all-card-deck'
 
 const GamePage = () => {
   return (
     <>
       {/* cards that players put on the field */}
       <PlayerCardList />
+      <AllCardDeckDisplay/>
     </>
   )
 }
