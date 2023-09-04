ef9981e360f63416e80181da6274caf30e63a53e
dimkahitrec <armormozg@mail.ru>
2023-09-04 21:22:47 +0300

    add variable with useState

--- src/containers/all-card-deck/AllCardDeck.tsx
+++ src/containers/all-card-deck/AllCardDeck.tsx
@@ -1,41 +1,40 @@
-import { type AllCardDeck as AllCardDeckProps } from './types'
-import styles from './styles.module.scss'
-               
-const AllCardDeck = ({ id, title, counter }: AllCardDeckProps) => {
-    return (
-        <div className={styles.AllCardDeck}>
-          <div className={styles.AllCardDeckExtend_before1}></div> 
-          <div className={styles.AllCardDeckExtend_before2}></div>   
-          <div className={styles.AllCardDeckExtend_before3}></div>    
-          <div className={styles.textCenter}>{counter}</div>                  
-        </div>
-    )
+import { type AllCardDeck as AllCardDeckProps } from "./types"
+import { useState } from "react"
+import styles from "./styles.module.scss"
+
+const AllCardDeck = ({ id, title }: AllCardDeckProps) => {
+  const [counterNumber, setCounter] = useState(25)
+  return (
+    <div className={styles.AllCardDeck}>
+      <div className={styles.AllCardDeckExtend_before1}></div>
+      <div className={styles.AllCardDeckExtend_before2}></div>
+      <div className={styles.AllCardDeckExtend_before3}></div>
+      <div className={styles.textCenter}>{counterNumber}</div>
+    </div>
+  )
 }
 
 const AllCardDeckMap: AllCardDeckProps[] = [
-    {
-      id: '1',
-      title: 'Добор',
-      counter: 25
-    }
+  {
+    id: "1",
+    title: "Добор",
+    counter: NaN,
+  },
 ]
 
 // display a card-deck
 const AllCardDeckDisplay = () => {
-    return (
-      <div>
-        {AllCardDeckMap.map((deckCards) => (
-          <AllCardDeck key={deckCards.id} {...deckCards} />
-        ))}
-      </div>
-    )
-  }
-
-export {
-    AllCardDeckDisplay
+  return (
+    <div>
+      {AllCardDeckMap.map((deckCards) => (
+        <AllCardDeck key={deckCards.id} {...deckCards} />
+      ))}
+    </div>
+  )
 }
-  
+
+export { AllCardDeckDisplay }
+
 export default {
-    AllCardDeckDisplay
+  AllCardDeckDisplay,
 }
-
