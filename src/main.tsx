8f1ee54e2cd66ec490e9978236a8c3a3d34d8723
dimkahitrec <armormozg@mail.ru>
2023-09-12 21:41:39 +0300

    _

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/index.tsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes/index.tsx"
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
