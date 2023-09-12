31ed5ff382159014260889e9d096c40dc28bb226
dimkahitrec <armormozg@mail.ru>
2023-09-12 21:27:46 +0300

    import wonloosestate component

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,3 +1,4 @@
+import WonLooseState from "../../containers/won-loose-state/WonLooseState"
 import { PlayerCardList, playerCards } from "../../containers/player-card-list"
 import HistoryFieldCard from "../../containers/history-field-card/HistoryFieldCard"
 import { AllCardDeckDisplay } from "../../containers/all-card-deck"
@@ -7,6 +8,7 @@ const GamePage = () => {
   return (
     <>
       {/* cards that players put on the field */}
+      <WonLooseState />
       <PlayerCardList />
       <HistoryFieldCard />
       <AllCardDeckDisplay />
