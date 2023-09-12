56b630538768c2affebb60f4943434f3f5fc10f1
dimkahitrec <armormozg@mail.ru>
2023-09-12 21:22:54 +0300

    add path to wonlosescreen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { GamePage } from '../pages/game/GamePage'
+import { createBrowserRouter } from "react-router-dom"
+import { GamePage } from "../pages/game/GamePage"
+import WonLooseScreen from "../pages/game/WonLooseScreen"
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <GamePage />
-  }
+    path: "/",
+    element: <GamePage />,
+  },
+  {
+    path: "/WonLooseScreen",
+    element: <WonLooseScreen />,
+  },
 ])
 
-export {
-  router
-}
+export { router }
