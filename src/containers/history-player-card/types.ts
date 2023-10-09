60f0fb159ab98e3d3b24608e34eec37e5b0dc8e6
dimkahitrec <armormozg@mail.ru>
2023-10-09 21:01:55 +0300

    in prograss

---
+++ src/containers/history-player-card/types.ts
@@ -0,0 +1,9 @@
+type HistoryCard = {
+  id: string
+  title: string
+  description: string
+  year: number
+  bc: boolean // before christ. indicates if the inventiona was created before christ
+}
+
+export type { HistoryCard }
