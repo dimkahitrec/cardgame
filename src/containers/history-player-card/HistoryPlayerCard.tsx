4ee36773bb32e0a78ab9217a1975bd3319a98983
dimkahitrec <armormozg@mail.ru>
2023-10-12 19:35:12 +0300

    _

--- src/containers/history-player-card/HistoryPlayerCard.tsx
+++ src/containers/history-player-card/HistoryPlayerCard.tsx
@@ -1,30 +1,24 @@
-import { type HistoryCard as HistoryPlayerCardProps } from './types'
-import styles from './styles.module.scss'
+import { type HistoryCard as HistoryPlayerCardProps } from "./types"
+import styles from "./styles.module.scss"
 
-const HistoryPlayerCard = ({ id, year, title, description }: HistoryPlayerCardProps) => {
+const HistoryPlayerCard = ({
+  id,
+  year,
+  title,
+  description,
+}: HistoryPlayerCardProps) => {
   return (
     <div className={styles.historyPlayerCard}>
-      <div className={styles.photo}>
-      </div>
+      <div className={styles.photo}></div>
       <div className={styles.wrapTitleYear}>
-        <h5>
-          {title}
-        </h5>
-        <h5>
-          {year}
-        </h5>
+        <h5>{title}</h5>
+        <h5>{year}</h5>
       </div>
-      <p className={styles.description}>
-        {description}
-      </p>
+      <p className={styles.description}>{description}</p>
     </div>
   )
 }
 
-export type {
-  HistoryPlayerCardProps
-}
+export type { HistoryPlayerCardProps }
 
-export {
-  HistoryPlayerCard
-}
+export { HistoryPlayerCard }
